982f9e3291ad14c64ba1fb64412d60f5649a028a
establers <establers@naver.com>
2024-02-27 00:59:09 +0900

    config 살짝 수정

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+from tkinter import filedialog, messagebox
+
+import yaml
+
 def format_config (config, indent=0) :
     """
     yaml 파일을 예쁘게 출력하기 위한 함수
@@ -13,5 +18,20 @@ def format_config (config, indent=0) :
             lines.append(f"{prefix}{key} : {value}")
     
     return "\n".join(lines)
+
+def open_config():
+    py_dir = os.path.dirname(__file__)
+    config_dir = os.path.join(py_dir, "ezcov_config.yaml")
+    try : 
+        with open(config_dir,'r') as yaml_file: 
+            config = yaml.safe_load(yaml_file)
+            # formatted_config = format_config(config)
+            # config_button_tooltip = CTkToolTip(get_config_button, delay=0.05, message=f'{formatted_config}', justify="left",  fg_color="transparent")            
+            # messagebox.showinfo("Get Config", f"설정을 정상적으로 가져왔습니다.")
+    except FileNotFoundError :
+        messagebox.showerror("Config File Not Found", f'{config_dir}\n설정 파일 찾기 실패. \n같은 경로에 설정 파일이 없습니다.')
+        return None
+    return config
+
 
 
