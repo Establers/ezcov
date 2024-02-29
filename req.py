cf16f99be3c8c152b329205f41d49665789b4076
Establers <44702967+Establers@users.noreply.github.com>
2024-02-29 15:57:30 +0900

    Update req.py

--- req.py
+++ req.py
@@ -4,7 +4,7 @@ from requests.auth import HTTPBasicAuth
 def check_server_status(url) : 
     # print("Check url : " + url)
     try :
-        response = requests.get(url, timeout=5)
+        response = requests.get(url, timeout=1)
         if response.status_code == 200 : 
             return True
         
